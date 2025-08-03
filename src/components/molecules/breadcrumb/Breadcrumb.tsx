df35a5da4c34dec3c31b537111f56dd8f1188b56
cudiousidy <idairaalemanquintana@gmail.com>
2025-08-03 15:02:11 +0100

    [UPDATE] Breadcrumb component stories file

--- src/components/molecules/breadcrumb/Breadcrumb.tsx
+++ src/components/molecules/breadcrumb/Breadcrumb.tsx
@@ -1,6 +1,7 @@
 import { cn } from '@/lib/utils';
 import { DynamicIcon, type IconName } from 'lucide-react/dynamic';
 import type { FC } from 'react';
+import React from 'react';
 import Link from '../../atoms/link';
 import { type BreadcrumbProps, breadcrumbVariants } from './types';
 import { useBreadcrumb } from './useBreadcrumb';
@@ -18,35 +19,48 @@ const Breadcrumb: FC<BreadcrumbProps> = ({ ...props }) => {
     shadow,
     size,
     startContent,
-    variant
+    variant,
+    classText
   } = useBreadcrumb({ ...props });
 
   return (
-    <nav>
-      <ul className={cn('w-auto', className, breadcrumbVariants({ variant, size, rounded, shadow }))}>
+    <nav aria-label='Breadcrumb'>
+      <ol className={cn('w-auto', className, breadcrumbVariants({ variant, size, rounded, shadow }))}>
         {processedItems.map((item, index) => (
-          <>
-            <li className={cn('flex')} key={`breadcrumb-${index}`}>
+          <React.Fragment key={`breadcrumb-${index}`}>
+            <li className='flex items-center'>
               {isBreadcrumbItem(item) ? (
                 <>
-                  {startContent && <DynamicIcon name={startContent as IconName} />}
-                  <Link title={item.title} href={item.href} target={item.target} size={size}>
+                  {startContent && (
+                    <DynamicIcon name={startContent as IconName} className={classText(props.colorText ?? '')} />
+                  )}
+                  <Link
+                    title={item.title}
+                    href={item.href}
+                    target={item.target}
+                    size={size}
+                    className={classText(props.colorText ?? '')}
+                  >
                     {item.title}
                   </Link>
-                  {endContent && <DynamicIcon name={endContent as IconName} />}
+                  {endContent && (
+                    <DynamicIcon name={endContent as IconName} className={classText(props.colorText ?? '')} />
+                  )}
                 </>
               ) : (
                 <span>{item}</span>
               )}
             </li>
-            <li key={`separator-${index}`}>
+            <li>
               {!hideSeparator && index < processedItems.length - 1 && (
-                <span className='mx-1'>{renderSeparator(separator)}</span>
+                <span className={`px-1 ${classText(props.colorText ?? '')}`} aria-hidden={separator !== '/'}>
+                  {separator && renderSeparator(separator)}
+                </span>
               )}
             </li>
-          </>
+          </React.Fragment>
         ))}
-      </ul>
+      </ol>
     </nav>
   );
 };
