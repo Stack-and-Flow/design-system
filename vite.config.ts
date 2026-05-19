7e98a3b9f6cbd2a7290ce76eb8c8896d4429ff5e
Enrique Gomez <egdev6@gmail.com>
2026-05-19 21:14:50 +0200

    fix(tsconfig): remove stale path aliases (#182)

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ const aliases = {
   '@atoms': path.resolve(__dirname, './src/components/atoms'),
   '@molecules': path.resolve(__dirname, './src/components/molecules'),
   '@organisms': path.resolve(__dirname, './src/components/organisms'),
-  '@templates': path.resolve(__dirname, './src/components/templates'),
+  '@hooks': path.resolve(__dirname, './src/hooks'),
   '@styles': path.resolve(__dirname, './src/styles'),
   '@utils': path.resolve(__dirname, './src/utils')
 };
